948606b401fa2e60a3919499a69045f725d95164
sagar <sagarnavghare9@gmail.com>
2020-06-19 17:08:11 +0530

    Create a game using jquery

---
+++ Simon-Game/game.js
@@ -0,0 +1,20 @@
+var buttonColours= ["red","blue","green","yellow"];
+
+
+var gamePattern =[];
+
+function nextSequence(){
+   randomNumber = Math.floor((Math.random() * 4) );
+
+  
+var randomChosenColour =buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
+
+
+
